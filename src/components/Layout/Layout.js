34c24a9bf36c84000b0edb52e51fe86442ffa6fb
AndreasBrochs <andreas.brochs@outlook.com>
2020-10-26 16:03:48 +0100

    side drawer for mobile

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,14 +2,16 @@ import React from 'react';
 import Aux from '../../hoc/Auxiliary';
 import classes from './Layout.module.css'
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const layout = (props) => (
     <Aux>
-<Toolbar />
-<main className={classes.Content}>
-    {props.children}
-</main>
-</Aux>
+        <Toolbar />
+        <SideDrawer/>
+        <main className={classes.Content}>
+            {props.children}
+        </main>
+    </Aux>
 );
 
 export default layout;
