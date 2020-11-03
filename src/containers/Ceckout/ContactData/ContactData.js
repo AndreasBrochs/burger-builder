7d610bc62e0f3cc8fc501e8f54886d337b27190d
AndreasBrochs <andreas.brochs@outlook.com>
2020-11-03 07:53:55 +0100

    re-created the form

--- src/containers/Ceckout/ContactData/ContactData.js
+++ src/containers/Ceckout/ContactData/ContactData.js
@@ -3,6 +3,7 @@ import Button from '../../../components/UI/Button/Button';
 import classes from './ContactData.module.css';
 import axios from '../../../axios-orders';
 import Spinner from '../../../components/UI/Spinner/Spinner';
+import Input from '../../../components/UI/Input/Input';
 
 
 class ContactData extends Component {
@@ -47,10 +48,10 @@ class ContactData extends Component {
 
     render() {
         let form = ( <form>
-            <input className={classes.Input} type="text" name="name" placeholder="Your Name" />
-            <input className={classes.Input} type="email" name="email" placeholder="Your Mail" />
-            <input className={classes.Input} type="text" name="street" placeholder="Your street" />
-            <input className={classes.Input} type="text" name="ZipCode" placeholder="Your ZipCode" />
+            <Input inputtype="input" type="text" name="name" placeholder="Your Name" />
+            <Input inputtype="input" type="email" name="email" placeholder="Your Mail" />
+            <Input inputtype="input" type="text" name="street" placeholder="Your street" />
+            <Input inputtype="input" type="text" name="ZipCode" placeholder="Your ZipCode" />
             <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
         </form>);
         if(this.state.loading) {
