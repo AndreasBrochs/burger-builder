1371d3843b0780c64f6256910c1af9c61552a960
AndreasBrochs <andreas.brochs@outlook.com>
2020-11-03 09:50:54 +0100

    form is now working and sending data to database

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -6,7 +6,7 @@ import classes from './CheckoutSummary.module.css';
 const checkoutSummary = (props) => {
     return(
         <div className={classes.CheckoutSummary}>
-            <h1>Enjoy your tasty burger</h1>
+            <h1>Confirm your tasty burger</h1>
             <div style={{width: '100%', margin: 'auto'}}>
                 <Burger ingredients={props.ingredients}/>
             </div>
