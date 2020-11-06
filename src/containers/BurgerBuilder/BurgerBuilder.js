0a156087238eceb49fbb2cfef9e8c8f12a7893be
AndreasBrochs <andreas.brochs@outlook.com>
2020-11-06 12:00:31 +0100

    added redux thunk

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -23,13 +23,13 @@ class BurgerBuilder extends Component {
     }
 
     componentDidMount() {
-        // axios.get('https://burgerbuilder-b.firebaseio.com/ingredients.json')
-        // .then(response => {
-        //     this.setState( {ingredients: response.data});
-        // })
-        // .catch(error => {
-        //     this.setState({error: true})
-        // });
+        axios.get('https://burgerbuilder-b.firebaseio.com/ingredients.json')
+        .then(response => {
+            this.setState( {ingredients: response.data});
+        })
+        .catch(error => {
+            this.setState({error: true})
+        });
     }
 
     updatePurchaseState (ingredients) {
